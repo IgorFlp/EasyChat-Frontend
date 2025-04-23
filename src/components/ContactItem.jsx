f12fd052be3c19c2222464a8c07a8b22251cb8ff
IgorFlp <igor.felipe.13@hotmail.com>
2025-04-22 21:25:19 -0300

    ContactItem create from messagens

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-const ContactItem = () => {
+const ContactItem = (messages) => {
+  messages = messages.messages;
+  console.log(messages);
   return (
     <>
       <div className="contact_item">
@@ -10,14 +12,15 @@ const ContactItem = () => {
           className="contact_item_img"
         />
         <div className="contact_item_texts">
-          <label className="contact_item_texts-name">Nome do contato</label>
+          <label className="contact_item_texts-name">{messages[0].name}</label>
           <div className="contact_item_texts_preview">
             <label className="contact_item_texts_preview-status">OK</label>
             <label className="contact_item_texts_preview-message">
-              Ultima mensagem asdouahsoduiashdoaidhaodisaho
-              aiosjdhioashdoashdoiashdoiashdoaidho
+              {messages[messages.length - 1].text}
             </label>
-            <label className="contact_item_texts_preview-time">12:00</label>
+            <label className="contact_item_texts_preview-time">
+              {messages[messages.length - 1].timestamp}
+            </label>
           </div>
         </div>
       </div>
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import ContactItem from "./ContactItem";
-
+import messages from "../assets/messages.json";
 const ContactList = () => {
+  const groupedMessages = messages.reduce((acc, msg) => {
+    if (!acc[msg.number]) {
+      acc[msg.number] = [];
+    }
+    acc[msg.number].push(msg);
+    return acc;
+  }, {});
+  const groupedArray = Object.values(groupedMessages);
   return (
     <>
-      <ContactItem />
-      <ContactItem />
-      <ContactItem />
-      <ContactItem />
-      <ContactItem />
-      <ContactItem />
+      {groupedArray.map((group, index) => {
+        const lastMessage = group[group.length - 1];
+        return <ContactItem key={index} messages={group} />;
+      })}
     </>
   );
 };
