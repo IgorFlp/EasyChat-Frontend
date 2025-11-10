b8ca3d42e7457e45f7f84233a29983369c31ea95
IgorFlp <igor.felipe.13@hotmail.com>
2025-11-10 15:48:23 -0300

    Added policies page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Login from "./pages/Login" 
+import Login from "./pages/Login";
 import ChatPage from "./pages/chatpage";
 import "./App.css";
+import PrivacyPolicy from "./pages/policies";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,11 +13,18 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />}/>
-          <Route path="/chats" element={<ProtectedRoute><ChatPage /></ProtectedRoute>}/>
+          <Route path="/" element={<Login />} />
+          <Route path="/policies" element={<PrivacyPolicy />} />
+          <Route
+            path="/chats"
+            element={
+              <ProtectedRoute>
+                <ChatPage />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
-      
     </>
   );
 }
